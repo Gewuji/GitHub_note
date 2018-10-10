26717dd69ddc403a4c7678735347fd8303725c23
gewuji <20921815@qq.com>
2018-10-10 17:28:05 +0800

    Update ControlSlider.cs

---
+++ ControlSlider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlSlider : MonoBehaviour
+{
+    public Slider slider;
+
+    public void ResetValue()
+    {
+        slider.value = 0;
+    }
+
+    public void ChangeValue(float value)
+    {
+        slider.value += value;
+        //Debug.Log("进度条参数:" + slider.value);
+    }
+
+}
