ed964bc717d4007c5eedd1a45ac51a0de2e2c75b
gewuji <20921815@qq.com>
2018-10-15 15:57:42 +0800

    Update CS_InternalEventSystem.cs

---
+++ CS_InternalEventSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 专门处理协程结束以后的委托
+/// </summary>
+public delegate void notification();
+
+public class CS_InternalEventSystem
+{
+
+
+}
